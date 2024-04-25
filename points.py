3e100b10f0646554f776627483f9b787a5586311
Dekac123 <dejan.milosevic181@gmail.com>
2024-04-25 15:42:04 +0200

    1

---
+++ points.py
@@ -0,0 +1,21 @@
+
+
+class Point(object):
+
+
+    def __init__(self, x, y, z, k, ):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.closest_neighbors = []
+
+
+    def knn(self, neighbors):
+        for i in range(len(neighbors)):
+            pass
+
+
+    @staticmethod
+    def calculate_knn(n, k , neighboars):
+
+        Point.closest_neighbors.append()
